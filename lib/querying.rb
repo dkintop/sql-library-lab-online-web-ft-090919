680eb3e39fe565fa0209a76988800c4d2a3a4512
Doug Kintop <dkintop@hotmail.com>
2019-10-09 15:20:19 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -33,7 +33,8 @@ def select_series_title_with_most_human_characters
   ON characters.series_id = series.id
   GROUP BY series.title, species
   HAVING characters.species = 'human'
-  ORDER BY COUNT(species) DESC;"
+  ORDER BY COUNT(species) DESC
+  LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
