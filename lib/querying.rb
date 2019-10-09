dd2ecd0712942962e9eee642bb6c3eb36642b5ec
Doug Kintop <dkintop@hotmail.com>
2019-10-09 15:10:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,7 +20,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM series INNER JOIN subgenres"
+  FROM series INNER JOIN authors"
 end
 
 def select_series_title_with_most_human_characters
