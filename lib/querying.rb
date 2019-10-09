8f067109504ee766aab6ca377f7b09ffa90dd8df
Doug Kintop <dkintop@hotmail.com>
2019-10-09 15:25:40 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,5 +41,5 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(name)
   FROM character_books INNER JOIN character
-  ON characters.id = character_books."
+  ON characters.id = character_books.character_id"
 end
