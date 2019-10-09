944d83dc5f10f6d0ebbda9776b21857349fa4c96
Doug Kintop <dkintop@hotmail.com>
2019-10-09 14:31:32 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,9 +4,15 @@ CREATE TABLE series (
 id INTEGER PRIMARY KEY,
 author_id INTEGER,
 subgenre_id INTEGER,
-title TEXT);
+title TEXT
+);
 
 CREATE TABLE subgenres(
 id INTEGER PRIMARY KEY,
 name TEXT 
+);
+
+CREATE TABLE authours (
+id INTEGER PRIMARY KEY,
+name TEXT
 );
