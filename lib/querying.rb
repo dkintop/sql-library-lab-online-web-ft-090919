aed19f9d6dfbef41d99c53c617f137da41654e04
Doug Kintop <dkintop@hotmail.com>
2019-10-09 15:06:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,8 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(species) FROM "
+  "SELECT species, COUNT(species) FROM characters
+  ORDER BY COUNT(species)"
 end
 
 def select_name_and_series_subgenres_of_authors
