cff969f08854ec7f90ad4a8f14afc104e2c9141d
Doug Kintop <dkintop@hotmail.com>
2019-10-09 15:19:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,7 +31,8 @@ def select_series_title_with_most_human_characters
   "SELECT series.title
   FROM series INNER JOIN characters
   ON characters.series_id = series.id
-  GROUP BY series.title, species;"
+  GROUP BY series.title, species
+  HAVING ;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
