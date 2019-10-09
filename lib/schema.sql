b21ba849d296e217525b2b177fde34c220eee70b
Doug Kintop <dkintop@hotmail.com>
2019-10-09 14:52:00 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -35,6 +35,6 @@ author_id INTEGER
 
 CREATE TABLE character_books(
 id INTEGER PRIMARY KEY,
-character_id INTEGER FOREIGN KEY,
-book_id INTEGER FOREIGN KEY);
+character_id INTEGER ,
+book_id INTEGER );
 
